002d9a601305fb9c06379ed2ff6424dfe812cd43
Malifarge <malifarge.thomas9@gmail.com>
2022-08-22 16:24:01 +0200

    associations

---
+++ associations.js
@@ -0,0 +1,22 @@
+const associations = [
+    {
+        name: "Les restos du coeur",
+        Slogan: "ON COMPTE SUR VOUS",
+        description: " Les restos du coeur ont pour but « d’aider et d’apporter une assistance bénévole aux personnes démunies, notamment dans le domaine alimentaire par l’accès à des repas gratuits, et par la participation à leur insertion sociale et économique, ainsi qu’à toute action contre la pauvreté sous toutes ses formes ».",
+        image: "https://dons.restosducoeur.org/cdn.iraiser.eu/yj9k53ZvSG82oqJBwPTI3w==/default/origin/logorestosducoeurhd5.png"
+    },
+    {
+        name: "unicef",
+        Slogan: " Cet enfant a besoin de vos talents ! ",
+        description: "L’UNICEF travaille dans les endroits les plus inhospitaliers du monde pour atteindre les enfants et les adolescents les plus défavorisés. Pour défendre les droits de chaque enfant, où qu’il soit. Dans plus de 190 pays et territoires, ils mettent tout en œuvre pour aider les enfants à vivre, à s’épanouir et à réaliser leur potentiel, de la petite enfance à l’adolescence.",
+        image: "https://i.pinimg.com/originals/24/bb/38/24bb38268caffa8a18fdede24fd5dff9.png"
+    },
+    {
+        name: "konexio",
+        Slogan: "Faire du numérique une opportunité pour tous.tes",
+        description: "Konexio est une association et un organisme de formation qui forme aux compétences numériques - des plus basiques aux plus avancées - afin de faciliter l'inclusion socioprofessionnelle.",
+        image: "https://www.carenews.com/sites/default/files/styles/large_actualites/public/articles/d9a7b8eb42833ea06a43d853949d60f925fb3f37_0.jpg?itok=9U9TYu0s"
+    }
+]
+
+module.exports = associations
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const port = 5000
+
+const associationsRoutes = require('./routes/associations')
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.use('/associations', associationsRoutes)
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`)
+})
